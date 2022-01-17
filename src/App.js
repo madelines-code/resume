42e7936e8f14cf7a889595af716ff1cfc974f1ba
Madeline Armstrong <armstrong.madeline@gmail.com>
2022-01-17 09:50:48 -0700

    sterter

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Madeline</h1>
+      <h2>Work History</h2>
+      <h3>Marketing Coordinator</h3>
+      <p>Worked here at the county</p>
     </div>
   );
 }
