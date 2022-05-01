4f8749c962d2c535218b8ba7ccf0549f1056942d
Madeline Armstrong <armstrong.madeline@gmail.com>
2022-05-01 12:36:23 -0400

    unused tags

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
-import { Button, Grid, GridColumn, GridRow, Icon, Menu } from 'semantic-ui-react'
+import { Icon, Menu } from 'semantic-ui-react'
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default class MenuExampleBasic extends Component {
